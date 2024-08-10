bbe651ec4b28a2e95d56ddedb8fc753e723b14c0
Ormel <60385109+ormelflores@users.noreply.github.com>
2024-08-10 16:51:28 +0800

    Implement user authentication

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AuthenticateUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    // Login user and create access token
+    public function store(AuthenticateUserRequest $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! Auth::attempt($credentials))
+        {
+            return $this->failedLoginAttemptResponse();
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful.',
+            'data' => $this->loginSuccessfulResponse($user, $token),
+        ], 201);
+    }
+
+    // Logout user and revoke current access token
+    public function logout(): JsonResponse
+    {
+        if (Auth::check())
+        {
+            Auth::user()->currentAccessToken()->delete();
+        }
+
+        return response()->json([
+            'message' => 'Logout successful.',
+        ], 201);
+    }
+
+    // Failed login attempt response
+    protected function failedLoginAttemptResponse(): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Invalid login credentials.',
+            'errors' => [
+                'details' => 'These credentials do not match our records.',
+            ],
+        ], 422);
+    }
+    
+    // Successful response
+    protected function loginSuccessfulResponse(User $user, string $token): array
+    {
+        return [
+            'user' => [
+                'email' => $user->email,
+                'name' => $user->name,
+            ],
+            'accessToken' => $token,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::post('/login', 'store')->middleware('guest')->name('api.user.authenticate');
+        Route::post('/logout', 'logout')->middleware('auth:sanctum')->name('api.user.logout');
+    });
+});
